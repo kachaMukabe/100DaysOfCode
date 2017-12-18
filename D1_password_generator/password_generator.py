b22ff2a2d9397c38aae812815470d1288eaefbc1
Kacha Mukabe <kachamukabe@gmail.com>
2017-12-19 00:17:49 +0200

    Day 1:password generator done

---
+++ D1_password_generator/password_generator.py
@@ -0,0 +1,20 @@
+import random
+
+dictionary = open('dict.txt', 'r')
+w_dict = [line for line in dictionary]
+
+
+def generate_password():
+    password = ''
+    word_count = 0
+
+    while word_count < 12:
+        word = random.choice(w_dict)
+        if word not in password:
+            password = password + word
+            word_count += 1
+    password = password.replace('\n', ' ')
+    return password
+
+
+print(generate_password())
